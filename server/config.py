6ebeeae3a50bd6100cb5815e362d5cfc0230b4aa
amenabe22 <bdere12345@gmail.com>
2023-03-04 14:34:20 -0800

    fix

--- server/config.py
+++ server/config.py
@@ -4,7 +4,7 @@ load_dotenv()
 
 MODE = os.getenv("MODE")
 TEST_BOT_TOKEN = os.getenv("TEST_BOT_TOKEN")
-PROD_BOT_TOKEN = os.getenv("TEST_BOT_TOKEN")
+PROD_BOT_TOKEN = os.getenv("PROD_BOT_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 CHANNEL_URL = os.getenv("CHANNEL_URL")
 MOD_CHAN_ID = os.getenv("MOD_CHAN_ID")
